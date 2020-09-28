ce3f315a7c11bf1deab7e305f6485c5be5a293e9
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-28 14:34:25 -0700

    added .ejs template for urls

--- express_server.js
+++ express_server.js
@@ -13,8 +13,15 @@ app.get("/", (req,res) => {
   res.send("Hello!");
 });
 
+/* Do I Need This?
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+*/
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
