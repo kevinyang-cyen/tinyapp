ac439cc0d4508ebffeea9b6b16c4b2676e11f8aa
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-28 16:28:21 -0700

    added a /urls_new page

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -13,20 +15,27 @@ app.get("/", (req,res) => {
   res.send("Hello!");
 });
 
-/* Do I Need This?
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-*/
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
@@ -36,3 +45,7 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listen on port ${PORT}`);
 });
+
+function generateRandomString() {
+
+}
