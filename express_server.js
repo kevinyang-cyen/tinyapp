c3e904692f748d775b27e936071cd2ccb38ea3bc
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-29 12:52:09 -0700

    addded a delete feature

--- express_server.js
+++ express_server.js
@@ -19,6 +19,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -45,8 +49,9 @@ app.post("/urls", (req, res) => {
   res.redirect(req.body.longURL);  // Redirect to new page
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
